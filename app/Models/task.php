260497fb75e106c718f7514ec63c900d84c61a4b
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-10-30 21:35:23 +0700

    Konsep Model dan Cara Membuatnya

---
+++ app/Models/task.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class task extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tasks';
+    // protected $fillable = []; -> Mendefinisan apa saja yang akan di isi
+
+    protected $guarded = []; //-> Memilih apa aja yang tidak boleh isi
+}

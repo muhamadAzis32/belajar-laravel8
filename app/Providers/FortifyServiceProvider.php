47460b11228f8e422651ffc9dcdc93f55ed1b1d9
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-11-02 09:48:12 +0700

    Mengaktifkan fitur login

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -49,5 +49,9 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::registerView(function () {
             return view('auth.register');
         });
+
+        Fortify::loginView(function () {
+            return view('auth.login');
+        });
     }
 }
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('main')
+    <div class="mt-5 mx-auto" style="width: 380px">
+        <div class="card">
+            <div class="card-body">
+                <form action="{{ url('login') }}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="" class="form-label">Email </label>
+                        <input name="email" type="email" class="form-control" value="{{ old('email') }}">
+                        <span class="text-danger">
+                            @error('email')
+                                {{ $message }}
+                            @enderror
+                        </span>
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">Password </label>
+                        <input name="password" type="password" class="form-control" value="{{ old('password') }}">
+                        <span class="text-danger">
+                            @error('password')
+                                {{ $message }}
+                            @enderror
+                        </span>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection

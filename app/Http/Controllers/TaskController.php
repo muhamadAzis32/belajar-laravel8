6e00f440270c890877a11025f4d93b87ce4560de
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-11-03 07:21:13 +0700

    Implementasi Middleware Role Dari Data User

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -13,6 +13,7 @@ class TaskController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('is_admin');
     }
 
     public function index(Request $request)

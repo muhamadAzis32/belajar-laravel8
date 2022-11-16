3d3065b36eab1f47f090bcc93006bc08633e3da6
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-11-16 08:30:33 +0700

    update middleware

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,12 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+Route::middleware(['auth'])->group(function () {
+    Route::get('/tasks', [TaskController::class, 'index']);
+});
+
 Route::get('/', [HomeController::class, 'index']);
-Route::get('/tasks', [TaskController::class, 'index']);
+// Route::get('/tasks', [TaskController::class, 'index']);
 Route::get('/tasks/create', [TaskController::class, 'create']);
 Route::get('/tasks/{id}', [TaskController::class, 'show']);
 Route::post('/tasks', [TaskController::class, 'store']);
